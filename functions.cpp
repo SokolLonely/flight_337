0c891d005d62d87c557d0d82c8b58bb7bff88bac
sokolLonely <simonryab@gmail.com>
2025-11-26 11:30:18 -0700

    welcoming message

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,11 @@
 //non-class functions for output and other things
+using namespace std;
+#include <iostream>
+void printWelcomeMessage() {
+    cout << "FMAS Version 1.0" << endl;
+    cout << "Term Project — Flight Management Application System" << endl;
+    cout << "Produced by Group# 139" << endl;
+    cout << "Members: Ryabinkin, Simon; ElSayed, Abdelrahman; Boucher, Maxime" << endl;
+    cout << endl;
+    cout << "<<< PRESS RETURN TO CONTINUE >>>" << endl;
+}
