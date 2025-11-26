c0869de7a0f3373f6b9f98e0175004cd30206152
sokolLonely <simonryab@gmail.com>
2025-11-26 11:34:17 -0700

    output functions

--- functions.cpp
+++ functions.cpp
@@ -9,3 +9,22 @@ void printWelcomeMessage() {
     cout << endl;
     cout << "<<< PRESS RETURN TO CONTINUE >>>" << endl;
 }
+void printChoicePrompt() {
+    cout<<"Please select one the following options:"<<endl;
+
+cout<<"1. Select a flight"<<endl;
+
+cout<<"2. Display Flight Seat Map."<<endl;
+
+cout<<"3. Display Passengers Information."<<endl;
+
+cout<<"4. Add a New Passenger."<<endl;
+
+cout<<"5. Remove an Existing Passenger"<<endl;
+
+cout<<"6. Save data"<<endl;
+
+cout<<"7. Quit."<<endl;
+
+cout<<"Enter your choice: (1, 2, 3, 4, 5, 6, or 7)";
+}
